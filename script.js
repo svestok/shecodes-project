2100a8b794fc7f1e37a5df4722b40d372b9e8a47
svestok <zhygalick@outlook.com>
2022-10-18 16:56:39 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text");
+  let h1 = document.querySelector("h1");
+  if (searchInput.value) {
+    h1.innerHTML = `${searchInput.value}`;
+  } else {
+    h1.innerHTML = null;
+    alert(`write a city`);
+  }
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let time = document.querySelector("#time");
+time.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function showPosition(position) {
+  let apiKey = "aa7d32afda2734eabb756b599b9677d1";
+  let city = document.querySelector("#search-text").value;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showWeather);
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.querySelector("#place");
+button.addEventListener("click", showPosition);
