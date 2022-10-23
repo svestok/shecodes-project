17980564d2ade25893512141f3ddada69879cfa5
svestok <zhygalick@outlook.com>
2022-10-23 11:13:29 +0200

    update script

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function showWeather(response) {
 }
 
 function showPosition(position) {
-  let apiKey = "...";
+  let apiKey = "55fa6bb8ce9106cc7e720db05b9549dd";
   let city = document.querySelector("#search-text").value;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(showWeather);
