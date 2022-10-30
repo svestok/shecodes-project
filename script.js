57c879c34a2d1afd3c905df750258e4f9acba2fd
svestok <zhygalick@outlook.com>
2022-10-30 22:10:51 +0100

    icons

--- script.js
+++ script.js
@@ -41,6 +41,12 @@ function showWeather(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
   celsiusTemperature = response.data.main.temp;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function showPosition(position) {
