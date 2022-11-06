61676f41cf2451b590ec6a2911411e943f196a14
svestok <zhygalick@outlook.com>
2022-11-06 14:11:44 +0100

    fake data forecast

--- script.js
+++ script.js
@@ -77,3 +77,27 @@ let fahrenheitLink = document.querySelector("#far");
 fahrenheitLink.addEventListener("click", showFarTemp);
 let celsiusLink = document.querySelector("#cel");
 celsiusLink.addEventListener("click", showCelTemp);
+
+function showForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+    <img src="http://openweathermap.org/img/wn/50d@2x.png"
+    alt=""
+    width="40"/>
+    <div class="forecast-temperature">
+    <span class="max">20°</span>
+    <span class="min">15°</span>
+    </div>
+    </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+showForecast();
